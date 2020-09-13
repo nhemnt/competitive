47bee900b4eb4b2b68954e4ba723c85030a5868b
hemant negi <nhemnt@gmail.com>
2020-09-14 02:00:05 +0530

    solve picking number problem

---
+++ c++/Picking Numbers/using_two_loops.cpp
@@ -0,0 +1,22 @@
+
+int pickingNumbers(vector<int> a) {
+    int max =0, innerMax;
+    for(int i =0; i< a.size(); i++){
+        innerMax = 0;
+        for(int j=0; j< a.size(); j++){
+            if(a[i] - a[j] < 2 && a[i] - a[j] >= 0){
+                cout<<"i ="<<a[i] << " j = " <<a[j] <<"\n";
+                cout<< "before = " <<innerMax<<"\n";
+                innerMax++;
+                cout<< "after = " <<innerMax<<"\n";;
+                cout<<"*******"<<"\n";
+            }
+        }
+        cout<<"\n";
+        if(innerMax > max ){
+            cout<<"innermax =" << innerMax << " : "<< "i = "<<i<<"\n";
+            max = innerMax;
+        }
+    }
+    return max;
+}
