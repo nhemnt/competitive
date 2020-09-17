10a29d87b2e3fe6729cdba22a1d2a8ab27afa549
hemant negi <nhemnt@gmail.com>
2020-09-18 02:15:30 +0530

    solve Designer pdf viewer problem with map

---
+++ c++/Designer PDF Viewer/usingMap.cpp
@@ -0,0 +1,20 @@
+int designerPdfViewer(vector<int> h, string word) {
+
+    std::map<char, int> words;
+
+    char albhabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; 
+    for(char i{}; i< h.size(); i++){
+        words[albhabet[i]] =  h[i];
+    }
+
+    int max =0;
+    for(int i =0; i<word.length(); i++){
+        int value = words.find(word[i])->second;
+        if(value > max){
+            max = value;
+        }
+    }
+
+    return max * word.length();
+
+}
